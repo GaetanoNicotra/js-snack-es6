260e0f638c3280bb9bc8a75984191da159bca33e
GaetanoNicotra <nicotragaetano10@gmail.com>
2025-03-28 15:45:53 +0100

    [UPD] creato l'array di oggetti del primo snack

---
+++ js/script.js
@@ -0,0 +1,38 @@
+// SNACK NUMERO 1: BICICLETTE
+
+// Creo un array di oggetti che conterrà il nome e il peso di ciascuna bicicletta
+
+const roadBike = [
+    {
+        name: 'Pinarello',
+        weight: 6.2
+    },
+    {
+        name: 'Bianchi',
+        weight: 7.2
+    },
+    {
+        name: 'Santa Cruz',
+        weight: 6
+    },
+    {
+        name: 'Specialized',
+        weight: 8
+    },
+    {
+        name: 'Cannodale',
+        weight: 5.1
+    },
+    {
+        name: 'Trek',
+        weight: 8.6
+    },
+    {
+        name: 'Colnago',
+        weight: 7.6
+    },
+    {
+        name: 'Scott',
+        weight: 5.9
+    }
+]
