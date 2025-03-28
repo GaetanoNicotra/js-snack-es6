4e8d7bb160f2f56c449e35e6b53966025c9f304a
GaetanoNicotra <nicotragaetano10@gmail.com>
2025-03-28 17:51:15 +0100

    [UPD] creato l'array di oggetti per le squadre di calcio

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 // SNACK NUMERO 1: BICICLETTE
 
-// Creo un array di oggetti che conterrà il nome e il peso di ciascuna bicicletta
+// Creo un array di oggetti che conterrà il nome e il peso di 8 biciclette
 
 const roadBike = [
     {
@@ -40,14 +40,72 @@ const roadBike = [
 // voglio stampare in console la bici con il peso minore
 // creo un ciclo for
 
+// dichiaro una variabile che conterrà il peso di ogni bici ad ogni giro del ciclo
 // dichiaro una variabile che conterrà il peso minore
+
 let min = roadBike[0].weight;
 let minWeight;
-for (let i = 0; i < roadBike.length; i++) {
 
+for (let i = 0; i < roadBike.length; i++) {
+    //verifico quale bici ha il peso miniore confrontando il peso ogni bici dell'array
     if (min > roadBike[i].weight) {
         minWeight = roadBike[i];
         min = roadBike[i].weight;
     }
 }
+// stampo il risultato in console
 console.log(minWeight)
+
+
+
+// SNACK NUMERO 2: SQUADRE DI CALCIO
+
+/* creo un array di oggetti che conterrà 8 squadre di calcio della serie A, 
+ciascuna delle quali avrà tre proprietà: nome, punti fatti, falli subiti.
+I punti fatti e i falli subiti saranno generati randomicamente*/
+
+const teams = [
+    {
+        name: 'Juventus',
+        punti: 0,
+        falli: 0
+    },
+    {
+        name: 'Milan',
+        punti: 0,
+        falli: 0
+        
+    },
+    {
+        name: 'Catania',
+        punti: 0,
+        falli: 0
+    },
+    {
+        name: 'Napoli',
+        punti: 0,
+        falli: 0
+    },
+    {
+        name: 'Inter',
+        punti: 0,
+        falli: 0
+    },
+    {
+        name: 'Atalanta',
+        punti: 0,
+        falli: 0
+    },
+    {
+        name: 'Roma',
+        punti: 0,
+        falli: 0
+    },
+    {
+        name: 'Lazio',
+        punti: 0,
+        falli: 0
+    }
+]
+
+console.log(teams)
